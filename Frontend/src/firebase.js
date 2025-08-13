4fd0f7c361defbf80e3efe8810a8d5eed1d5cc89
harrish ragavan <163574184+harrishragavan@users.noreply.github.com>
2025-08-13 15:51:58 +0530

    firebase.js

--- Frontend/src/firebase.js
+++ Frontend/src/firebase.js
@@ -4,13 +4,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "your_api_key",
-  authDomain: "your_auth_domain",
-  projectId: "your_projectId",
-  storageBucket: "your_storageBucket",
-  messagingSenderId: "your_messagingSenderId",
-  appId: "your_appId",
-  measurementId: "your_measurementId"
+  apiKey: "AIzaSyB6p_Hjb-MG4lpBQqClMOmuHSZ7GqGL_-A",
+  authDomain: "chatbot-efec8.firebaseapp.com",
+  projectId: "chatbot-efec8",
+  storageBucket: "chatbot-efec8.firebasestorage.app",
+  messagingSenderId: "363038804794",
+  appId: "1:363038804794:web:7b9cf924f1bd0b5860de03",
+  measurementId: "G-0VFMQGBZJX"
 };
 
 const app = initializeApp(firebaseConfig);
